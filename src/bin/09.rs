3a0c23ce523a6583ad0cefaa547527ab3dbf03a7
braidonw <braidon@braidonwhatley.com>
2024-12-10 00:15:00 +1100

    day9 p1

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,34 +1,20 @@
-use std::collections::VecDeque;
-
 use miette::Error;
 
 advent_of_code::solution!(9);
 
-struct Blocks(VecDeque<Block>);
+struct Blocks(Vec<Option<File>>);
+struct File(u64);
 
 impl Blocks {
-    fn checksum(&self) -> Option<u128> {
-        let mut checksum: u128 = 0;
+    fn checksum(&self) -> Option<u64> {
+        let mut checksum: u64 = 0;
         for (i, block) in self.0.iter().enumerate() {
-            match block {
-                Block::File(id) => checksum += (id * i as u64) as u128,
-                Block::EmptySpace => {}
+            if let Some(File(id)) = block {
+                checksum += (i as u64) * (*id as u64);
             }
         }
 
         Some(checksum)
-    }
-
-    fn empty_space_idxs(&self) -> Vec<usize> {
-        self.0
-            .iter()
-            .enumerate()
-            .filter(|(_idx, block)| match block {
-                Block::EmptySpace => true,
-                _ => false,
-            })
-            .map(|(idx, _)| idx)
-            .collect()
     }
 }
 
@@ -38,22 +24,16 @@ impl std::fmt::Debug for Blocks {
             .0
             .iter()
             .map(|block| match block {
-                Block::File(id) => id.to_string(),
-                Block::EmptySpace => ".".to_string(),
+                Some(File(id)) => id.to_string(),
+                None => ".".to_string(),
             })
             .collect();
         write!(f, "{}", block_strings.join(" "))
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-enum Block {
-    File(u64),
-    EmptySpace,
-}
-
 fn parse_input(input: &str) -> Blocks {
-    let mut blocks = VecDeque::new();
+    let mut blocks = Vec::new();
 
     let mut parsing_file = true;
     let mut current_file_id = 0;
@@ -61,13 +41,13 @@ fn parse_input(input: &str) -> Blocks {
         let c_num = c.to_digit(10).map(|d| d as usize).expect("Failed to parse");
         if parsing_file {
             for _ in 0..c_num {
-                blocks.push_back(Block::File(current_file_id));
+                blocks.push(Some(File(current_file_id)));
             }
 
             current_file_id += 1;
         } else {
             for _ in 0..c_num {
-                blocks.push_back(Block::EmptySpace);
+                blocks.push(None);
             }
         }
 
@@ -78,23 +58,21 @@ fn parse_input(input: &str) -> Blocks {
 }
 
 fn compress(blocks: &mut Blocks) -> Result<(), Error> {
-    'outer: for blank_idx in dbg!(blocks.empty_space_idxs()) {
-        if blank_idx >= blocks.0.len() {
-            break 'outer;
+    let mut front_idx = 0;
+    let mut back_idx = blocks.0.len() - 1;
+
+    while front_idx < back_idx {
+        while front_idx < back_idx && blocks.0[front_idx].is_some() {
+            front_idx += 1;
         }
 
-        // pop off the empty spaces at the back
-        while let Some(Block::EmptySpace) = blocks.0.back() {
-            blocks.0.pop_back();
+        while back_idx > front_idx && blocks.0[back_idx].is_none() {
+            back_idx -= 1;
         }
-        let block_to_insert = blocks
-            .0
-            .pop_back()
-            .expect("Failed to get a reference to the back el of the blocks");
 
-        // insert the block at the blank index
-        blocks.0.insert(blank_idx, block_to_insert);
-        blocks.0.remove(blank_idx + 1);
+        if front_idx < back_idx {
+            blocks.0.swap(front_idx, back_idx);
+        }
     }
 
     Ok(())
@@ -103,7 +81,6 @@ fn compress(blocks: &mut Blocks) -> Result<(), Error> {
 pub fn part_one(input: &str) -> Option<u32> {
     let mut blocks = parse_input(input);
     let _ = compress(&mut blocks);
-    dbg!(&blocks);
 
     let checksum = blocks.checksum().expect("Failed to calculate checksum");
     dbg!(&checksum);
@@ -112,6 +89,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let mut blocks = parse_input(input);
     None
 }
 
@@ -128,6 +106,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
